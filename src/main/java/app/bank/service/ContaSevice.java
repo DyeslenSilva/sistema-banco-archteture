4e78b2887cdf1fb2c7db83ad955458684f38754b
DyeslenSilva <dyeslensilva97@gmail.com>
2022-11-03 16:24:01 -0300

    alterando servicos de conta corrente

--- src/main/java/app/bank/service/ContaSevice.java
+++ src/main/java/app/bank/service/ContaSevice.java
@@ -1,5 +1,36 @@
 package app.bank.service;
 
-public class ContaSevice {
+import org.springframework.stereotype.Service;
 
+import antlr.collections.List;
+import app.bank.model.ContaPF;
+import app.bank.model.ContaPJ;
+import app.bank.repo.ContaPFRepo;
+import app.bank.repo.ContaPJRepo;
+
+@Service
+public class ContaSevice {
+	
+	private ContaPFRepo contaPFRepo;
+	private ContaPJRepo contaPJRepo;
+	
+	public boolean abrirContaPF(ContaPF contaPF) {
+		boolean saveCliente = contaPFRepo.save(contaPF) != null;
+		return saveCliente;
+	}
+	
+	public boolean abrirContaPJ(ContaPJ contaPJ) {
+		boolean saveCliente = contaPJRepo.save(contaPJ) != null;
+		return saveCliente;
+	}
+	
+	public java.util.List<ContaPF> getAllContaPF(){
+		return contaPFRepo.findAll();
+	}
+	
+	public java.util.List<ContaPJ> getAllContaPJ(){
+		return contaPJRepo.findAll();
+	}
+	
+	
 }
