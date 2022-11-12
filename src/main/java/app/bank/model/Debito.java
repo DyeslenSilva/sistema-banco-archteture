e287dc78afe778f033742c290286647eb347aa64
DyeslenSilva <dyeslensilva97@gmail.com>
2022-11-12 16:47:31 -0300

    refatorando o metodo de consulta de saldo

--- src/main/java/app/bank/model/Debito.java
+++ src/main/java/app/bank/model/Debito.java
@@ -19,15 +19,22 @@ public class Debito extends Cartoes {
 	private int[] nCartao;
 	
 	
-	public static double consultaSaldo(ClientePF clientePF, ClientePJ clientePJ, int senha) {
-		 double saldo=Cartoes.getSaldoDebito();
-		 if (contaPF.setClientePF(clientePF.getCpf()) && contaPF.setSenha(senha)) {
-			 	return	contaPF.setSaldo(saldo);
-		 }
-		 
-		 if(contaPJ.setClientePJ(clientePJ.getCNPJ())&& contaPJ.setSenha(senha)) {
-			 return contaPJ.setSaldo(saldo);
-		 }
+	public static double consultaSaldoPF(ClientePF clientePF, int senha) {
+		double saldo = Cartoes.getSaldoDebito();
+		if(contaPF.setClientePF(clientePF.getCpf())&& contaPF.setSenha(senha)) {
+			return contaPF.setSaldo(saldo);
+		}
 		return saldo;
 	}
+	
+	
+	public static double consultaSaldoPJ(ClientePJ clientePJ, int senha) {
+		double saldo = Cartoes.getSaldoDebito();
+		if(contaPJ.setClientePJ(clientePJ.getCNPJ())&& contaPJ.setSenha(senha)) {
+			return contaPJ.setSaldo(saldo);
+		}else {
+			return saldo;
+		}
+	}
+	
 }
